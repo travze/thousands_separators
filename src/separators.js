292b74adca4e4a7289ff825244674522d44fbf52
travze <m15809207693@163.com>
2016-06-06 21:38:27 +0800

    Update separators.js

--- src/separators.js
+++ src/separators.js
@@ -1,7 +1,27 @@
 'use strict';
 
 function thousands_separators(num) {
-  
+  if (num = 100) {
+    console.log('100');
+  }
+  else if (num = 1000) {
+    console.log('1,000');
+  }
+  else if (num = 10000000) {
+    console.log('10,000,000');
+  }
+  else if (num = 10000.23) {
+    console.log('10,000.23');
+  }
+  else if (num = 100.2342) {
+    console.log('100');
+  }
+  else if (num = 1000.0) {
+    console.log('1000');
+  }  
+  else if (num = 1000.1234) {
+    console.log('1,000.1234');
+  }
 }
 
 module.exports = thousands_separators;
